080546f26c4fd474e75fd5c81437d15fb413f22e
gajul <gajul@172.16.57.12>
2018-02-15 14:14:52 -0500

    Initial

---
+++ FirstWebProject/src/com/First/webapp/UserLogin.java
@@ -0,0 +1,47 @@
+package com.First.webapp;
+
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class UserLogin implements Servlet {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		System.out.println("<<<<<<<<<<<<<$$$$>>>>>>>>>>");
+		String usrname=request.getParameter("uname");
+		String pswrd=request.getParameter("pwd");
+		System.out.println("<<<<<<<"+usrname+">>>>>$$<<<<<"+pswrd);
+		
+	}
+
+}
