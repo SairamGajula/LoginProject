c53c4ef7d113d81c1fb46259261bf7ae808c38ab
gajul <gajul@172.16.57.12>
2018-02-15 14:28:07 -0500

    small build path change

--- FirstWebProject/src/com/First/webapp/UserLogin.java
+++ FirstWebProject/src/com/First/webapp/UserLogin.java
@@ -13,25 +13,25 @@ public class UserLogin implements Servlet {
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public ServletConfig getServletConfig() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public String getServletInfo() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void init(ServletConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
